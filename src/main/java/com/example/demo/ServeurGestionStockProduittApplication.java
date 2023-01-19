c28279823685c761454f40c42c9da5eacf47b922
unknown <you@example.com>
2023-01-19 08:15:15 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionStockProduittApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduittApplication.class, args);
+		System.out.println("hiiii");
+	}
+
+}
---
+++ src/test/java/com/example/demo/ServeurGestionStockProduittApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduittApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduittApplication.class, args);
+	}
+
+}
